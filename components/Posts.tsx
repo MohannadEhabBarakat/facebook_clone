3f99bf0e90f70bf4b694024cf1780718e99cb143
Mohannad Ehab Barakat <hannod98@yahoo.com>
2023-01-23 08:31:53 +0200

    add DB to services

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,41 +1,32 @@
 // 'use client';
 
-import React, {ReactElement}  from "react";
-import { useSession } from "next-auth/react"
-import { useCollection } from 'react-firebase-hooks/firestore'
-import { db, storage } from '../firebase';
+import React, { ReactElement } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import { getPosts } from "../services/firebase";
 import { collection, orderBy, query } from "firebase/firestore";
 import Post from "./Post";
 
-function Posts():ReactElement{
-    const { data: session } = useSession()
-    const [realtimePosts, loading, error] = useCollection(
-        query(collection(db, "posts"), orderBy('timestamp', 'desc'))
-    )
+function Posts(): ReactElement {
+  const [realtimePosts, loading, error] = getPosts();
+  
 
-    // console.log(error)
-    // console.log(realtimePosts)
-
-    return(
-        
-
-        <div className="mt-5 w-11/12 sm:w-8/12 block mx-auto items-center">
-        {
-            realtimePosts?.docs.map(post=>{
-                
-                return <Post
-                    key={post.id}
-                    name={post.data().name}
-                    massage={post.data().massage}
-                    email={post.data().email}
-                    timestamp={post.data().timestamp}
-                    image={post.data().image}
-                    postImage={post.data().postImage}
-                />  
-            })
-        }
-        </div>
-    )
-
+  return (
+    <div className="mt-5 w-11/12 sm:w-8/12 block mx-auto items-center">
+      {realtimePosts?.docs.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            name={post.data().name}
+            massage={post.data().massage}
+            email={post.data().email}
+            timestamp={post.data().timestamp}
+            image={post.data().image}
+            postImage={post.data().postImage}
+          />
+        );
+      })}
+    </div>
+  );
 }
 export default Posts;
